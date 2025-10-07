bcf21a37e70bce9f67f770e368e742336ede9064
Plane <bf.plane@proton.me>
2025-10-07 16:35:17 +0200

    feat: improve refund function

--- gelato/web3-functions/refund/index.ts
+++ gelato/web3-functions/refund/index.ts
@@ -17,13 +17,22 @@ const abi = parseAbi([
   "function refund(address _table, uint256 _round) external",
 ]);
 
-const query = `
+const tableAbi = parseAbi([
+  "function roundSpinned(uint256) external view returns (uint256)",
+]);
+
+const makeQuery = (yesterdayTimestamp: number, limit: number) => `
 {
   bets(where: {status: 1, table_: {interval: 0}}) {
     id
   }
   
-  rounds(where: {status: 2, started_lt: $yesterday}, orderBy: started, orderDirection: asc, first: 1) {
+  roundsStatus1: rounds(where: {status: 1, started_lt: ${yesterdayTimestamp}}, orderBy: started, orderDirection: asc, first: ${limit}) {
+    table 
+    round
+    started
+  }
+  roundsStatus2: rounds(where: {status: 2, started_lt: ${yesterdayTimestamp}}, orderBy: started, orderDirection: asc, first: ${limit}) {
     table 
     round
     started
@@ -31,26 +40,38 @@ const query = `
 }
 `;
 
+type Response = {
+  roundsStatus1: { round: bigint, table: Address, started: bigint }[],
+  roundsStatus2: { round: bigint, table: Address, started: bigint }[],
+  bets: { id: Address }[]
+}
+
 Web3Function.onRun(async (context: Web3FunctionContext) => {
   const { userArgs, gelatoArgs, secrets } = context;
 
   const url = await secrets.get("ROULETTE_URL") || "";
   const rpc = await secrets.get("RPC_URL") || "";
+  const limit = Number(await secrets.get("LIMIT")) || 100;
   const roulette = userArgs.roulette as Address;
   const client = createPublicClient({
     chain: gelatoArgs.chainId === 137 ? polygon : polygonAmoy,
     transport: http(rpc),
   });
-  const response = await request<{ rounds: { round: bigint, table: Address, started: bigint }[], bets: { id: Address }[] }>(url, query.replace("$yesterday", Math.floor(new Date(Date.now() - 24 * 60 * 60 * 1000).getTime() / 1000).toString()));
+  
+  const yesterdayTimestamp = Math.floor(new Date(Date.now() - 24 * 60 * 60 * 1000).getTime() / 1000);
+  const response = await request<Response>(url, makeQuery(yesterdayTimestamp, limit));
+  const roundsStatus1 = response.roundsStatus1;
+  const roundsStatus2 = response.roundsStatus2;
+  const bets = response.bets;
 
   const data: Web3FunctionResultCallData[] = []
   let tries = 0;
   while (true) {
-    if (tries > response.bets.length) {
+    if (tries > bets.length) {
       break;
     }
     // get one random bet from bets
-    const randomBet = response.bets[Math.floor(Math.random() * response.bets.length)];
+    const randomBet = bets[Math.floor(Math.random() * bets.length)];
     if (randomBet === undefined) break;
     try {
       // check if bet is refundable(by calling simulateContract)
@@ -74,18 +95,70 @@ Web3Function.onRun(async (context: Web3FunctionContext) => {
       tries++;
     }
   }
-  // push each round from rounds
-  for (const round of response.rounds) {
-    data.push({
-      to: roulette,
-      data: encodeFunctionData({
+  
+  // since requested rounds (status=2) can be refunded 1 day after VRF REQEUST, we need to filter others
+  const roundsStatus2RoundSpinnedsMulticall = roundsStatus2.map((round) => ({
+    address: round.table,
+    abi: tableAbi,
+    functionName: "roundSpinned",
+    args: [round.round],
+  }));
+  const roundsStatus2RoundSpinnedsMulticallResults = await client.multicall({ contracts: roundsStatus2RoundSpinnedsMulticall });
+  for(let i = 0; i < roundsStatus2.length; i++) {
+    const round = roundsStatus2[i];
+    const roundSpinned = roundsStatus2RoundSpinnedsMulticallResults[i].status === 'success' ? Number(roundsStatus2RoundSpinnedsMulticallResults[i].result) : 0;
+    if (roundSpinned < BigInt(yesterdayTimestamp)) {
+      try {
+        await client.simulateContract({
+          address: roulette,
+          abi: abi,
+          functionName: "refund",
+          args: [round.table, round.round],
+        });
+        data.push({
+          to: roulette,
+          data: encodeFunctionData({
+            abi: abi,
+            functionName: "refund",
+            args: [round.table, round.round],
+          })
+        });
+        console.log(`Round ${round.round} on table ${round.table} (status 2) passed refund simulation`);
+      } catch (e) {
+        console.log(`Round ${round.round} on table ${round.table} (status 2) failed refund simulation:`, e);
+      }
+    }
+  }
+
+  // push each round that were not spinned (status=1)
+  for (const round of roundsStatus1) {
+    try {
+      await client.simulateContract({
+        address: roulette,
         abi: abi,
         functionName: "refund",
         args: [round.table, round.round],
-      })
-    })
+      });
+      data.push({
+        to: roulette,
+        data: encodeFunctionData({
+          abi: abi,
+          functionName: "refund",
+          args: [round.table, round.round],
+        })
+      });
+      console.log(`Round ${round.round} on table ${round.table} (status 1) passed refund simulation`);
+    } catch (e) {
+      console.log(`Round ${round.round} on table ${round.table} (status 1) failed refund simulation:`, e);
+    }
   }
 
+  if(data.length === 0) {
+    return {
+      canExec: false,
+      message: "No rounds to refund",
+    };
+  }
 
   return {
     canExec: true,
